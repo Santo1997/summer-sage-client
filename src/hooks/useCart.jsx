550ec2017723e7157d7e7aca01a1687addb88758
Santo1997 <hossainsanto2@gmail.com>
2023-06-11 13:24:08 +0600

    axios secure

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,17 +1,17 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
   const { user } = useContext(AuthContext);
+  const [axiosSecure] = useAxiosSecure();
+
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["cart", user?.email],
     queryFn: async () => {
-      const res = await fetch(
-        `http://localhost:5000/carts?user=${user?.email}`
-      );
-
-      return res.json();
+      const res = await axiosSecure(`/carts?user=${user?.email}`);
+      return res.data;
     },
   });
 
