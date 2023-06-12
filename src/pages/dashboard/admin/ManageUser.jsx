18332c65b0b80e4ba9b2a249018950a43387622b
Santo1997 <hossainsanto2@gmail.com>
2023-06-12 13:29:15 +0600

    manage user

--- src/pages/dashboard/admin/ManageUser.jsx
+++ src/pages/dashboard/admin/ManageUser.jsx
@@ -1,6 +1,34 @@
+import { toast } from "react-hot-toast";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
+
 const ManageUser = () => {
-  let img =
-    "https://i.ibb.co/19cZpB9/360-F-46340033-r4r-ZUll7-MDn-SXM4n-S87qoy-AKEg-BGXIKE.jpg";
+  const [cart, refetch] = useCart("users");
+  const [axiosSecure] = useAxiosSecure();
+
+  const accessDBHandle = (id, itmData, notify) => {
+    axiosSecure.put(`/updateUser/${id}`, itmData).then((response) => {
+      if (response.data.modifiedCount > 0) {
+        toast.success(notify);
+        refetch();
+      }
+    });
+  };
+
+  const handleInstractor = (itm) => {
+    const instractor = {
+      role: "instractor",
+    };
+    console.log(instractor);
+    accessDBHandle(itm._id, instractor, `${itm.name} Is Instractor Now`);
+  };
+  const handleAdmin = (itm) => {
+    const instractor = {
+      role: "admin",
+    };
+    console.log(instractor);
+    accessDBHandle(itm._id, instractor, `${itm.name} Is Admin Now`);
+  };
 
   return (
     <div className="overflow-x-auto">
@@ -8,38 +36,51 @@ const ManageUser = () => {
         <thead>
           <tr>
             <th>#</th>
-            <th>Student Image</th>
-            <th>Class Name</th>
-            <th>Student Email</th>
+            <th>User Image</th>
+            <th>User Name</th>
+            <th>User Email</th>
+            <th>User Role</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr>
-            <th>1</th>
-            <td>
-              <div className="avatar">
-                <div className="mask mask-squircle w-12 h-12">
-                  <img src={img} />
+          {cart.map((itm, index) => (
+            <tr key={itm._id}>
+              <th>{index + 1}</th>
+              <td>
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src={itm.img} />
+                  </div>
                 </div>
-              </div>
-            </td>
-            <td>
-              <h2 className="font-bold">Hart Hagerty</h2>
-            </td>
-            <td>
-              <h2 className="font-bold">Hart Hagerty</h2>
-            </td>
-            <td className="grid grid-rows-3 gap-2">
-              <button className="btn btn-outline btn-sm btn-success w-3/6 mx-auto">
-                Make Instructor
-              </button>
-              <button className="btn btn-outline btn-sm btn-error w-3/6 mx-auto">
-                Make Admin
-              </button>
-            </td>
-          </tr>
+              </td>
+              <td>
+                <h2 className="font-bold">{itm.name}</h2>
+              </td>
+              <td>
+                <h2 className="font-bold">{itm.email}</h2>
+              </td>
+              <td>
+                <h2 className="font-bold capitalize">{itm.role}</h2>
+              </td>
+              <td className="grid justify-center gap-2">
+                <button
+                  onClick={() => handleInstractor(itm)}
+                  className="btn btn-outline btn-xs btn-info"
+                  disabled={itm.role === "instractor" || itm.role === "admin"}
+                >
+                  Make Instructor
+                </button>
+                <button
+                  onClick={() => handleAdmin(itm)}
+                  className="btn btn-outline btn-xs btn-success"
+                  disabled={itm.role === "admin"}
+                >
+                  Make Admin
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
