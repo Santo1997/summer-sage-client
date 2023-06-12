6db3e0eac788a781e119541a8d6a85b42e56f979
Santo1997 <hossainsanto2@gmail.com>
2023-06-12 12:10:29 +0600

    adds terms managecls

--- src/pages/dashboard/admin/ManageCls.jsx
+++ src/pages/dashboard/admin/ManageCls.jsx
@@ -1,16 +1,24 @@
 import { toast } from "react-hot-toast";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const ManageCls = () => {
   const [cart, refetch] = useCart("allCourses");
+  const [clsID, setClsID] = useState(null);
   const [axiosSecure] = useAxiosSecure();
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  //TODO: confirm to Approve
-  const accessDBHandle = (id, itmData) => {
-    axiosSecure.put(`/updateStatus/${id}`, itmData).then((response) => {
+  const accessDBHandle = (id, itmData, notify) => {
+    axiosSecure.put(`/updateValue/${id}`, itmData).then((response) => {
       if (response.data.modifiedCount > 0) {
-        toast.success("Course Status Updated");
+        toast.success(notify);
         refetch();
       }
     });
@@ -18,110 +26,172 @@ const ManageCls = () => {
 
   const handleApprove = (id) => {
     const updateStatus = {
-      status: "confirm",
+      status: "approved",
     };
-    accessDBHandle(id, updateStatus);
+    accessDBHandle(id, updateStatus, "Course Status Updated");
   };
   const handleDeny = (id) => {
     const updateStatus = {
       status: "denied",
     };
-    accessDBHandle(id, updateStatus);
+    accessDBHandle(id, updateStatus, "Course Status Updated");
+  };
+
+  const onSubmit = (data) => {
+    const setFeedback = {
+      feedback: data.feedback,
+    };
+    accessDBHandle(clsID, setFeedback, "Feedback Send");
+    reset();
+    setClsID(null);
+  };
+
+  const handleFeedbackClick = (id) => {
+    setClsID(id);
+    window.my_modal_5.showModal();
   };
 
   return (
-    <div className="overflow-x-auto">
-      <table className="table text-center">
-        <thead>
-          <tr>
-            <th>Class Image</th>
-            <th>Class Name</th>
-            <th>Instructor Name</th>
-            <th>Instructor Email</th>
-            <th>Available seats</th>
-            <th>Price</th>
-            <th>Status</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map((itm) => (
-            <tr key={itm._id}>
-              <td>
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={itm.course_img} />
-                  </div>
-                </div>
-              </td>
-              <td>
-                <h2 className="font-bold">{itm.course_name}</h2>
-              </td>
-              <td>
-                <h2 className="font-bold">{itm.course_teacher.name}</h2>
-              </td>
-              <td>
-                <h2 className="font-bold">{itm.course_teacher.email}</h2>
-              </td>
-              <td>{itm.available_seats}</td>
-              <td>
-                $<span>{itm.course_price}</span>
-              </td>
-              <td>
-                {itm.status === "pending" ? (
-                  <h2 className="font-bold text-warning capitalize">
-                    {itm.status}
-                  </h2>
-                ) : (
-                  <>
-                    {itm.status === "confirm" ? (
-                      <h2 className="font-bold text-success capitalize">
-                        {itm.status}
-                      </h2>
-                    ) : (
-                      <h2 className="font-bold text-error capitalize">
-                        {itm.status}
-                      </h2>
-                    )}
-                  </>
-                )}
-              </td>
-              <td className="grid grid-rows-3 gap-2">
-                {itm.status === "pending" ? (
-                  <>
-                    <button
-                      onClick={() => handleApprove(itm._id)}
-                      className="btn btn-outline btn-xs btn-success"
-                    >
-                      Approve
-                    </button>
-                    <button
-                      onClick={() => handleDeny(itm._id)}
-                      className="btn btn-outline btn-xs btn-error"
-                    >
-                      Deny
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <button className="btn btn-xs" disabled="disabled">
-                      Approve
-                    </button>
-                    <button className="btn btn-xs" disabled="disabled">
-                      Deny
-                    </button>
-                  </>
-                )}
-
-                <button className="btn btn-outline btn-xs btn-info">
-                  Feedback
-                </button>
-              </td>
+    <>
+      <div className="overflow-x-auto">
+        <table className="table text-center">
+          <thead>
+            <tr>
+              <th>Class Image</th>
+              <th>Class Name</th>
+              <th>Instructor Name</th>
+              <th>Instructor Email</th>
+              <th>Available seats</th>
+              <th>Price</th>
+              <th>Status</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {cart.map((itm) => (
+              <tr key={itm._id}>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src={itm.course_img} />
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <h2 className="font-bold">{itm.course_name}</h2>
+                </td>
+                <td>
+                  <h2 className="font-bold">{itm.course_teacher.name}</h2>
+                </td>
+                <td>
+                  <h2 className="font-bold">{itm.course_teacher.email}</h2>
+                </td>
+                <td>{itm.available_seats}</td>
+                <td>{itm.feedback}</td>
+                <td>
+                  $<span>{itm.course_price}</span>
+                </td>
+                <td>
+                  {itm.status === "pending" ? (
+                    <h2 className="font-bold text-warning capitalize">
+                      {itm.status}
+                    </h2>
+                  ) : (
+                    <>
+                      {itm.status === "approved" ? (
+                        <h2 className="font-bold text-success capitalize">
+                          {itm.status}
+                        </h2>
+                      ) : (
+                        <h2 className="font-bold text-error capitalize">
+                          {itm.status}
+                        </h2>
+                      )}
+                    </>
+                  )}
+                </td>
+                <td className="grid grid-rows-3 gap-2">
+                  {itm.status === "pending" ? (
+                    <>
+                      <button
+                        onClick={() => handleApprove(itm._id)}
+                        className="btn btn-outline btn-xs btn-success"
+                      >
+                        Approve
+                      </button>
+                      <button
+                        onClick={() => handleDeny(itm._id)}
+                        className="btn btn-outline btn-xs btn-error"
+                      >
+                        Deny
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button className="btn btn-xs" disabled="disabled">
+                        Approve
+                      </button>
+                      <button className="btn btn-xs" disabled="disabled">
+                        Deny
+                      </button>
+                    </>
+                  )}
+                  <button
+                    onClick={() => handleFeedbackClick(itm._id)}
+                    className="btn btn-outline btn-xs btn-info"
+                  >
+                    Feedback
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <dialog id="my_modal_5" className="modal">
+          <div method="dialog" className="modal-box">
+            <button
+              htmlFor="my-modal-5"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => {
+                document.getElementById("my_modal_5").close();
+              }}
+            >
+              ✕
+            </button>
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black text-lg font-bold">
+                    Send Your Feedback:
+                  </span>
+                </label>
+                <textarea
+                  placeholder="Your feedback"
+                  className="input input-bordered h-24"
+                  {...register("feedback", {
+                    required: true,
+                    minLength: 12,
+                  })}
+                ></textarea>
+                {errors.feedback?.type === "required" && (
+                  <span className="mt-1 text-red-600">
+                    This field is required
+                  </span>
+                )}
+                {errors.feedback?.type === "minLength" && (
+                  <span className="mt-1 text-red-600">
+                    Feedback must be a 12 Characture long
+                  </span>
+                )}
+              </div>
+              <div className="form-control m-5 items-end">
+                <button className="btn btn-primary w-fit">Send</button>
+              </div>
+            </form>
+          </div>
+        </dialog>
+      </div>
+    </>
   );
 };
 
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -3,7 +3,6 @@ import { useForm } from "react-hook-form";
 import { AuthContext } from "../../provider/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
-// import { postToDB } from "../../utilities/apiFetch";
 
 const Signup = () => {
   const { createUser, updateUserProfile } = useContext(AuthContext);
